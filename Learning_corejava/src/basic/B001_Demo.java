fe64d2a535af028d2d807ff204bf4bf3b6fb61d4
chintan <chintan@gmail.com>
2025-04-10 12:21:49 +0530

    basic

---
+++ Learning_corejava/src/basic/B001_Demo.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class B001_Demo {
+	public static void main(String[] args) {
+
+		System.out.println("Hello java");
+		System.out.println("Hello Python");
+
+		
+		//int a12$_ = 10;
+		
+//		int myRollNumber = 10;
+		
+		int a = 10;
+		int static1 = 20;
+		
+	}
+}
