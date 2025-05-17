ee43e3efa40f62716ecff1b195c7467cf4f7d910
chintan <chintan@gmail.com>
2025-05-17 12:27:33 +0530

    all

---
+++ Learning_corejava/src/oops/O007_Encaptulation.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class O007_Encaptulation {
+	public static void main(String[] args) {
+		
+		
+		Emp e  =new Emp();
+		
+		e.setId(10);
+		System.out.println(e.getId());
+		
+		
+	}
+}
